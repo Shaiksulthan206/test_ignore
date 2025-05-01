0d7bccae0b7b2be04670c46440bd94cf36dd0f1c
Shaiksulthan206 <shaiksulthan20698@gmail.com>
2025-05-01 14:19:54 +0530

    Update demo.tf

--- demo.tf
+++ demo.tf
@@ -1,6 +1,6 @@
 resource "aws_instance" "demo" {
 
-    ami           = "ami-12345678" # Replace with a valid AMI ID
+    ami           = "ami-0f88e80871fd81e91" # Replace with a valid AMI ID
     instance_type = "t2.micro"
     
     tags = {
